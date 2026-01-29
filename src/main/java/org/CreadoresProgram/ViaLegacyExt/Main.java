d5054d4119d0640763e89012cf31bb1aea453894
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-29 13:23:27 -0600

    Create Main.java

---
+++ src/main/java/org/CreadoresProgram/ViaLegacyExt/Main.java
@@ -0,0 +1,8 @@
+package org.CreadoresProgram.ViaLegacyExt;
+import net.raphimc.viaproxy.plugins.ViaProxyPlugin;
+public class Main extends ViaProxyPlugin{
+  @Override
+  public void onEnable(){}
+  @Override
+  public void onDisable(){}
+}
