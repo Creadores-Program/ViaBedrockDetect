38d8e5ae2bc8d202e5161c650564f81d06004f44
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-02-15 10:58:54 -0600

    Update and rename Main.java to ViaProxyMain.java

---
+++ src/main/java/org/CreadoresProgram/ViaBedrockDetect/ViaProxyMain.java
@@ -0,0 +1,33 @@
+package org.CreadoresProgram.ViaBedrockDetect;
+import net.raphimc.viaproxy.plugins.ViaProxyPlugin;
+import net.raphimc.viaproxy.ViaProxy;
+import net.raphimc.viaproxy.plugins.events.ViaLoadingEvent;
+import net.raphimc.viabedrock.protocol.provider.SkinProvider;
+import com.viaversion.viaversion.api.Via;
+import net.lenni0451.lambdaevents.EventHandler;
+import java.util.Map;
+public class ViaProxyMain extends ViaProxyPlugin{
+  private String prefix = "[ViaBedrockDetect] ";
+  @Override
+  public void onEnable(){
+    System.out.println(prefix + "Loading ViaBedrockDetect...");
+    ViaProxy.EVENT_MANAGER.register(this);
+    System.out.println(prefix + "Done!");
+  }
+  @Override
+  public void onDisable(){
+    System.out.println(prefix + "Bye!");
+  }
+  @EventHandler
+  public void onViaLoading(ViaLoadingEvent event){
+    Via.getManager().getProviders().use(SkinProvider.class, new SkinProvider(){
+      @Override
+      public Map<String, Object> getClientPlayerSkin(final UserConnection user){
+        final Map<String, Object> claims = super.getClientPlayerSkin(user);
+        claims.put("DeviceOS", 7);
+        claims.put("DeviceModel", "ViaProxyBedrock");
+        return claims;
+      }
+    });
+  }
+}
