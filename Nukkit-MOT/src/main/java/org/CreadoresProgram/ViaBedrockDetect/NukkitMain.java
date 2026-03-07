33411e00ad3e909efe2f4a83bb7d3ae18844d2ed
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-03-06 20:40:06 -0600

    Create NukkitMain.java

---
+++ Nukkit-MOT/src/main/java/org/CreadoresProgram/ViaBedrockDetect/NukkitMain.java
@@ -0,0 +1,30 @@
+package org.CreadoresProgram.ViaBedrockDetect;
+import cn.nukkit.Player;
+import cn.nukkit.plugin.PluginBase;
+import java.util.List;
+import java.util.ArrayList;
+public class NukkitMain extends PluginBase{
+  private static NukkitMain instance;
+  public static NukkitMain getInstance(){
+    return instance;
+  }
+  public void onEnable(){
+    instance = this;
+    this.getLogger().info("§aDone!");
+  }
+  public void onDisable(){
+    this.getLogger().info("§cBye!");
+  }
+  public boolean isJavaPlayer(Player player){
+    return player.isJavaClient() || (player.getLoginChainData().getDeviceModel().equals("ViaProxyBedrock") && player.getLoginChainData().getDeviceOS() == 7);
+  }
+  public List<Player> getJavaPlayers(){
+    List<Player> javaPlayers = new ArrayList<>();
+    for(Player player : this.getServer().getOnlinePlayers().values()){
+      if(this.isJavaPlayer(player)){
+        javaPlayers.add(player);
+      }
+    }
+    return javaPlayers;
+  }
+}
